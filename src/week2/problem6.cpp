5fea01c454395a83bad048d2999addc099021ffd
suxrobjalolov <2110020@newuzbekistanuniversity.uz>
2025-06-05 17:02:45 +0500

    .

---
+++ src/week2/problem6.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    double S, d;
+    cout << "Enter the value of d: ";
+    cin>>d;
+    double pi=3.14, r=d/2;
+    S=4*pi*r*r;
+    cout << "Area of a sphere with the diameter of "<<d<<" is euqal to "<<S<<endl;
+
+    return 0;
+}//
+// Created by suxrobjalolov on 6/5/25.
+//
---
+++ src/week2/problem7.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a,b,c,d;
+    cin>>a>>b>>c>>d;
+    int sum1=a+b;
+    int sum2=c+d;
+    cout << "Sum of the first pair: "<<sum1<<"\n Sum of the second pair: "<<sum2<<"\n Is sum1 < sum2? "<<(sum1<sum2)<<"\n Is sum1 > sum2? "<<(sum1>sum2)<<endl;
+
+    return 0;
+}//
+// Created by suxrobjalolov on 6/5/25.
+//
