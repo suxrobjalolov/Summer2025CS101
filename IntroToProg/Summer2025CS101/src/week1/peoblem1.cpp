b44f7a59d35154549bc7dbf7cc729433d0de4188
Sukhrobjon Jalolov <114988727+suxrobjalolov@users.noreply.github.com>
2025-06-09 17:36:51 +0500

    Add files via upload

---
+++ IntroToProg/Summer2025CS101/src/week1/peoblem1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a;
+    int b;
+    cout<<"a is "; cin >> a;
+    cout<<"b is "; cin >> b;
+    cout<<"a+b is "<<a+b<<endl;
+
+    return 0;
+}
---
+++ IntroToProg/Summer2025CS101/src/week1/problem3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    double a, b;
+    cin >> a, b;
+    cout<<"Is a<b? " << (a<b) << endl;
+    cout<<"Is a>b? " << (a>b) << endl;
+    cout<<"Is a==b? " << (a==b) << endl;
+
+    return 0;
+}
