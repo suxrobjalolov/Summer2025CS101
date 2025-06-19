675a75b4741bb83b45cf32b4be4e831a16d923a2
Sukhrobjon Jalolov <114988727+suxrobjalolov@users.noreply.github.com>
2025-06-19 16:13:19 +0500

    Create Problem3.cpp

---
+++ src/week3/lab3/Problem3.cpp
@@ -0,0 +1,19 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+float series(float n, float i, float r){
+    if (n<=0){
+        return 0;
+    } else if(i<=n){
+        r=r+1/(i*(i+2));
+        return series(n, i+1, r);
+    }
+    return r;
+}
+int main() {
+    float i=1;
+    float n;
+    cin>>n;
+    float r=0;
+    cout<<series(n, i, r);
+}
